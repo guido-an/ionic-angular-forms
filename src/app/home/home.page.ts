4d305d158cc9990a7546c5ba757c3a6df429c6fc
Guido <carucciguido@gmail.com>
2020-03-04 16:17:05 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {
+  Validators,
+  NgForm,
+  FormBuilder,
+  FormGroup,
+  FormControl
+} from '@angular/forms'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
 
+/* Template Driven Form */
+/* export class HomePage {
   constructor() {}
 
+  login(form: NgForm) {
+    console.log(form, 'my form')
+  }
+} */
+export class HomePage {
+  // new instance
+  dataForm: FormGroup
+
+  // creates a new FormControl and FormGroup
+  constructor(formBuilder: FormBuilder) {
+    this.dataForm = formBuilder.group({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      password: ''
+    })
+  }
+  login(data) {
+    console.log(data.value, 'form.value')
+  }
 }
